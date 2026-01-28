7c11427f76c5ed4c7653f2e815428028c907e548
hyein89 <bihontai@gmail.com>
2026-01-29 01:16:53 +0700

    Update Header.js

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,4 +1,5 @@
 'use client';
+import { CONFIG } from '../config'; 
 import Link from 'next/link';
 
 export default function Header() {
@@ -9,7 +10,7 @@ export default function Header() {
           <svg viewBox="0 0 24 24" style={{width: '24px', height:'24px', fill:'currentColor'}}>
             <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/>
           </svg>
-          <span>EU.ORG<b>F-EMAIL</b></span>
+          <span>{CONFIG.siteName}</span>
         </Link>
       </div>
     </div>
